e6d2d938761b0cf12f407ea306df411e06337e47
antonio carlos <actorresjunior@gmail.com>
2020-06-05 03:22:41 -0300

    project and dba config created

---
+++ src/test/java/com/torresantonio/codeblog/CodeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresantonio.codeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
