c8ebb7d3bf50d5253f167b43718fa3ba1f018bc5
antonio carlos <actorresjunior@gmail.com>
2020-06-25 17:40:08 -0300

    validation message and summernote for text formatting

--- src/main/java/com/torresantonio/codeblog/controller/CodeblogController.java
+++ src/main/java/com/torresantonio/codeblog/controller/CodeblogController.java
@@ -48,6 +48,7 @@ public class CodeblogController {
 	@PostMapping(value="/newpost")
 	public String savePost (@Valid Post post, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatorios foram preenchidos!");
 			return "redirect:/newpost";
 		
 	}
